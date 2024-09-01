d9d0f53510c0c9f8748beeb72e362d9d42433203
ErikPeter2000 <erik.hurinek@gmx.co.uk>
2024-09-01 21:15:03 +0100

    Transposition bugfixes.

--- src/main/scala/com/core/ciphers/TranspositionCipher.scala
+++ src/main/scala/com/core/ciphers/TranspositionCipher.scala
@@ -2,27 +2,34 @@ package com.core.ciphers
 
 import com.core.cipherdata._
 
-/**
-  * The Transposition cipher is a simple form of encryption where the positions of the characters are changed based on a key of indices.  
-  * 
-  * Like all transposition ciphers, the index of coincidence is the same as the plaintext. The frequency distribution is also the same as the plaintext.
+/** The Transposition cipher is a simple form of encryption where the positions of the characters are changed based on a
+  * key of indices.
+  *
+  * Data should be padded to a multiple of the key size and made from digits 0 to key.size-1, else errors may occur. Use
+  * `data.padTo(targetLength, padCharacter)` to pad the data.
+  *
+  * Like all transposition ciphers, the index of coincidence is the same as the plaintext. The frequency distribution is
+  * also the same as the plaintext.
   */
 object TranspositionCipher extends BaseCipher[Char, Char, IndexedSeq[Int]] {
     def encrypt(data: CipherDataBlock[Char], key: IndexedSeq[Int]): CipherDataBlock[Char] = {
-        val outData = data.grouped(key.size).flatMap { group =>
-            key.indices.flatMap { i =>
-                group.lift(key(i))
-            }
-        }.toIndexedSeq
+        val columnSize = data.size / key.size
+        val columns = data.grouped(key.size).toSeq.transpose // split into columns
+        val outData = key.map { i =>
+            columns.lift(i).get
+        } // reorder columns
+        .flatten // flatten into a single sequence
         CipherDataBlock.createFrom(outData, data.alphabet)
     }
 
     def decrypt(data: CipherDataBlock[Char], key: IndexedSeq[Int]): CipherDataBlock[Char] = {
-        val outData = data.grouped(key.size).flatMap { group =>
-            key.indices.flatMap { i =>
-                group.lift(key.indexOf(i))
-            }
-        }.toIndexedSeq
+        val columnSize = data.size / key.size
+        val columns = data.grouped(columnSize).toSeq // split into columns
+        val outData =  key.indices.map { i =>
+            columns.lift(key.indexOf(i)).get
+        } // reorder columns
+        .transpose// transpose back to rows
+        .flatten // flatten into a single sequence
         CipherDataBlock.createFrom(outData, data.alphabet)
     }
 }
--- src/test/scala/com/core/ciphers/TranspositionCipherTests.scala
+++ src/test/scala/com/core/ciphers/TranspositionCipherTests.scala
@@ -6,40 +6,19 @@ import com.core.alphabets.UppercaseLetters
 
 class TranspositionCipherTest extends munit.FunSuite {
 
-    test("TranspositionCipher encrypts correctly with given key") {
-        val data = new CipherDataBlock("HELLO", UppercaseLetters)
+    test("TranspositionCipher.encrypt") {
+        val data = CipherDataBlock.create("BYGEORGEORWELLPARTONEITWASABRIGHTCOLDDAYINAPRILANDTHECLOCKSWERESTRIKINGTHIR")
+        val expected = "GELTTBTDAAEKEKHBRWAESGLIITOERGYGERIAHDNLHCRITORPNAIOYRDLWTNREOLOWRCAPNCSSII"
         val key = IndexedSeq(2, 0, 1, 4, 3)
         val result = TranspositionCipher.encrypt(data, key)
-        assertEquals(result.mkString, "LHEOL")
+        assertEquals(result.mkString, expected)
     }
 
-    test("TranspositionCipher decrypts correctly with given key") {
-        val data = new CipherDataBlock("LHEOL", UppercaseLetters)
+    test("TranspositionCipher.decrypt") {
+        val data = new CipherDataBlock("GELTTBTDAAEKEKHBRWAESGLIITOERGYGERIAHDNLHCRITORPNAIOYRDLWTNREOLOWRCAPNCSSII", UppercaseLetters)
+        val expected = "BYGEORGEORWELLPARTONEITWASABRIGHTCOLDDAYINAPRILANDTHECLOCKSWERESTRIKINGTHIR"
         val key = IndexedSeq(2, 0, 1, 4, 3)
         val result = TranspositionCipher.decrypt(data, key)
-        assertEquals(result.mkString, "HELLO")
-    }
-
-    test("TranspositionCipher handles empty input") {
-        val data = new CipherDataBlock("", UppercaseLetters)
-        val key = IndexedSeq(2, 0, 1)
-        val encryptedResult = TranspositionCipher.encrypt(data, key)
-        val decryptedResult = TranspositionCipher.decrypt(data, key)
-        assertEquals(encryptedResult.mkString, "")
-        assertEquals(decryptedResult.mkString, "")
-    }
-
-    test("TranspositionCipher handles non-matching key size") {
-        val data = new CipherDataBlock("ABCDE", UppercaseLetters)
-        val key = IndexedSeq(2, 1, 0)
-        val result = TranspositionCipher.encrypt(data, key)
-        assertEquals(result.mkString, "CBAED")
-    }
-
-    test("TranspositionCipher decrypts correctly with non-matching key size") {
-        val data = new CipherDataBlock("CBAED", UppercaseLetters)
-        val key = IndexedSeq(2, 1, 0)
-        val result = TranspositionCipher.decrypt(data, key)
-        assertEquals(result.mkString, "ABCDE")
+        assertEquals(result.mkString, expected)
     }
 }
