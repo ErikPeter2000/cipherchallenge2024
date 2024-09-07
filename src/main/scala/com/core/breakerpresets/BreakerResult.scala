32e3d71aae2c89b526d835de5449320effc9eeec
ErikPeter2000 <erik.hurinek@gmx.co.uk>
2024-09-07 17:36:18 +0100

    Xml "serialisation".

--- src/main/scala/com/core/breakerpresets/BreakerResult.scala
+++ src/main/scala/com/core/breakerpresets/BreakerResult.scala
@@ -11,5 +11,18 @@ class BreakerResult[T, K, V](
     val score: Double
 ) {
     def textData: String = outData.mkString
+    def toXml: String = {
+        val keyString = key match {
+            case seq: Seq[_] => seq.mkString(",")
+            case _ => key.toString
+        }
+        s"""<BreakerResult>
+           |  <inData>${inData.mkString}</inData>
+           |  <outData>${outData.mkString}</outData>
+           |  <cipherUsed>${cipherUsed.getClass.getName}</cipherUsed>
+           |  <key>$keyString</key>
+           |  <score>$score</score>
+           |</BreakerResult>""".stripMargin
+    }
     override def toString(): String = textData
 }
