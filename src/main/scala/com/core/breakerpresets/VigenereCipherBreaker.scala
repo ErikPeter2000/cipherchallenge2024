b1fbdc048c962ef4807940fae63c11b8074ec153
ErikPeter2000 <erik.hurinek@gmx.co.uk>
2024-09-14 17:46:46 +0100

    Comments for Vigenere cipher breaker and Kasiski test.

--- src/main/scala/com/core/breakerpresets/VigenereCipherBreaker.scala
+++ src/main/scala/com/core/breakerpresets/VigenereCipherBreaker.scala
@@ -9,26 +9,53 @@ import com.core.evolutionaryalgorithms.FitnessFunctions.EriksWordFitness
 import com.core.progressbar.ProgressBar
 
 object VigenereCipherBreaker extends BreakerPreset[Char, Seq[Char]] {
+    // The vulnerability of the Viginere cipher lies in the fact that if we know the key length 'n', we know that every nth letter has been encrypted with the same shift value. This means that we can treat each column as a Caesar cipher, and solve it using frequency analysis. 
     def break(data: String) = {
         break(CipherDataBlock.create(data, UppercaseLetters))
     }
     def break(data: CipherDataBlock[Char]) = {
+        // Get possible key lengths
         val kasiskiTest = KasiskisTest.calculate(data)
+
+        // Convert to a vector of key lengths, with the most frequent factors first. (We are testing all key lengths returned, so the sort is unnecessary)
         val bestLengths = kasiskiTest.keys.toVector.sortBy(x => -kasiskiTest(x))
+
+        // Setup a progress bar
         val progressBar = new ProgressBar(bestLengths.length, "VigenereCipherBreaker")
+
+        // Now generate some decryption attempts for each key length. We currently brute force all possible lengths, but this can be optimised by taking the top n. I didn't find this necessary because it's fast enough.
         val scoreKeyDataPairs = bestLengths.map { case length =>
-            val groups = data.grouped(length).toVector.dropRight(1).transpose
-            val shiftValues = groups.map { group =>
-                val shift = CaesarCipherBreaker.getKey(group)._1
-                UppercaseLetters(shift)
+            // Group the data into groups of the key length
+            val columns = data
+                .grouped(length)
+                .toVector
+                .dropRight(1) // drop in case the last group is not full length, otherwise transpose won't work
+                .transpose // Convert to a list of columns. Now, all elements of a column have been encrypted with the same shift value, so we can treat each column as a Caesar cipher, and is susceptible to frequency analysis.
+
+            // Get the shift value for each column by solving the Caesar cipher
+            val shiftValues = columns.map { group =>
+                val shift = CaesarCipherBreaker.getKey(group)._1 // get the value
+                UppercaseLetters(shift) // return the letter at that shift value.
             }
+
+            // Decrypt the data using the shift values taken from this key length
             val decrypted = VigenereCipher.decrypt(data, shiftValues)
+
+            // Measure the fitness of the decrypted text
             val score = EriksWordFitness(decrypted)
+
+            // Update the progress bar
             progressBar.increment()
+
+            // Return what we found
             (score, shiftValues, decrypted)
         }
         progressBar.finish()
+
+        // Get the best score
         val bestScore = scoreKeyDataPairs.maxBy(_._1)
+
+        // Return the best result
         new BreakerResult(
             inData = data,
             outData = bestScore._3,
