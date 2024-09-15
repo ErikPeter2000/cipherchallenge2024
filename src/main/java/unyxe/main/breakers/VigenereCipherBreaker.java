8bb94cf457494781a2d27b4e61f5325f52c70fa2
Unyxe <luckyboy7892official@gmail.com>
2024-09-15 19:41:58 +0100

    Implemented vigenere cipher breaker (hill climber, very fast but requires the period. The period can be found using the period analysers. Works even for short ciphertexts).

--- src/main/java/unyxe/main/breakers/VigenereCipherBreaker.java
+++ src/main/java/unyxe/main/breakers/VigenereCipherBreaker.java
@@ -60,5 +60,35 @@ public class VigenereCipherBreaker {
         output.isSuccessfull = (output.plainText!=null);
         return output;
     }
+    public static CipherBreakerOutput hillClimberAttack(String cipherText, int period){
+        CipherBreakerOutput output = new CipherBreakerOutput("VigenereCipher", cipherText);
+        output.fitness = FitnessCalculator.TetragramFitness(cipherText);
 
+
+        char[] key = "A".repeat(period).toCharArray();
+        while(true){
+            double oldFitness = output.fitness;
+            for(int i = 0; i < period;i++){
+                double maxFitness = -999999;
+                char bestLetter = key[i];
+                for(int j = 0; j < Constants.monogramCount; j++){
+                    key[i] = (char)(65+j);
+                    String keyString = new String(key);
+                    String text = VigenereCipher.decipher(cipherText, keyString);
+                    double newFitness = FitnessCalculator.TetragramFitness(text);
+                    if (newFitness > maxFitness) {
+                        maxFitness = newFitness;
+                        bestLetter = (char)(65+j);
+                    }
+                }
+                key[i] = bestLetter;
+            }
+            output.key = new String(key);
+            output.plainText = VigenereCipher.decipher(cipherText, output.key);
+            output.fitness = FitnessCalculator.TetragramFitness(output.plainText);
+            if(output.fitness == oldFitness){break;}
+        }
+        output.isSuccessfull = true;
+        return output;
+    }
 }
