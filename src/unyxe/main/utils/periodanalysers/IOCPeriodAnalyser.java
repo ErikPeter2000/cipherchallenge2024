508c70d99475c9fe557349c67b82b2ee15659819
Unyxe <luckyboy7892official@gmail.com>
2024-09-15 14:06:54 +0100

    Implemented IOC period analyser for poly-alphabetic substitution ciphers.

--- src/unyxe/main/utils/periodanalysers/IOCPeriodAnalyser.java
+++ src/unyxe/main/utils/periodanalysers/IOCPeriodAnalyser.java
@@ -1,4 +1,35 @@
 package main.utils.periodanalysers;
 
+import main.utils.Analyser;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class IOCPeriodAnalyser {
+    public static String[] splitText(String text, int n) {
+        String[] splitted = new String[n];
+        Arrays.fill(splitted, "");
+        for(int i = 0; i < text.length(); i++) {
+            splitted[i%n] += text.charAt(i);
+        }
+        return splitted;
+    }
+
+    public static double averageIOCOfSlices(String[] slices){
+        double averageIOC = 0;
+        for (String slice : slices) {
+            averageIOC += Analyser.getIndexOfCoincedence(slice, true);
+        }
+        return averageIOC/slices.length;
+    }
+
+    public static int[] guessPeriod(String cipherText){
+        ArrayList<Integer> periodList = new ArrayList<>();
+        for(int i = 1; i < 16; i++){
+            double averageIOC = averageIOCOfSlices(splitText(cipherText, i));
+            //System.out.println(i + ":" + averageIOC);
+            if(1.65<averageIOC){periodList.add(i);}
+        }
+        return periodList.stream().mapToInt(i -> i).toArray();
+    }
 }
