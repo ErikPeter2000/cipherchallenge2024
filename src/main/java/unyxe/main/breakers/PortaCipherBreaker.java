0a474f2540aabb298e2867e79f5c1e563f81aebf
Unyxe <luckyboy7892official@gmail.com>
2024-09-16 01:26:24 +0100

    Implemented Porta cipher breaker (bruteforce, dictionary, hill-climb) for modernized 1552 version.

--- src/main/java/unyxe/main/breakers/PortaCipherBreaker.java
+++ src/main/java/unyxe/main/breakers/PortaCipherBreaker.java
@@ -46,8 +46,77 @@ public class PortaCipherBreaker {
         }
         return keys;
     }
+    public static CipherBreakerOutput bruteforceBellaso1552(String cipherText, double maxLength){
+        CipherBreakerOutput output = new CipherBreakerOutput("PortaCipherBellaso1552", cipherText);
+        output.fitness = FitnessCalculator.TetragramFitness(cipherText);
+        for(int n = 1; n <= maxLength; n++){
+            String[] possibleKeys = generateKeysPorta(n);
+            for (String possibleKey : possibleKeys) {
+                String text = PortaCipher.decipherBellaco1552(cipherText, possibleKey);
+                double newFitness = FitnessCalculator.TetragramFitness(text);
+                if (newFitness > output.fitness) {
+                    output.fitness = newFitness;
+                    output.key = possibleKey;
+                    output.plainText = text;
+                }
+            }
+            System.out.println("Key-length " + n + " finished. Best: " + output.plainText);
+        }
+        output.isSuccessfull = (output.plainText!=null);
+        return output;
+    }
+
+    public static CipherBreakerOutput bruteforceWithWordlistBellaso1552(String cipherText){
+        CipherBreakerOutput output = new CipherBreakerOutput("PortaCipherBellaso1552", cipherText);
+        output.fitness = FitnessCalculator.TetragramFitness(cipherText);
+        for(int n = 0; n < Constants.wordlist.length; n++){
+            String key = Constants.wordlist[n];
+            String text = PortaCipher.decipherBellaco1552(cipherText, key);
+            double newFitness = FitnessCalculator.TetragramFitness(text);
+            if (newFitness > output.fitness) {
+                output.fitness = newFitness;
+                output.key = key;
+                output.plainText = text;
+            }
+        }
+        output.isSuccessfull = (output.plainText!=null);
+        return output;
+    }
+
+    public static CipherBreakerOutput hillClimberAttackBellaso1552(String cipherText, int period){
+        CipherBreakerOutput output = new CipherBreakerOutput("PortaCipherBellaso1552", cipherText);
+        output.fitness = FitnessCalculator.TetragramFitness(cipherText);
+
+
+        char[] key = "A".repeat(period).toCharArray();
+        while(true){
+            double oldFitness = output.fitness;
+            for(int i = 0; i < period;i++){
+                double maxFitness = -999999;
+                char bestLetter = key[i];
+                for(int j = 0; j < 26; j++){
+                    key[i] = (char)(65+j);
+                    String keyString = new String(key);
+                    String text = PortaCipher.decipherBellaco1552(cipherText, keyString);
+                    double newFitness = FitnessCalculator.TetragramFitness(text);
+                    if (newFitness > maxFitness) {
+                        maxFitness = newFitness;
+                        bestLetter = (char)(65+j);
+                    }
+                }
+                key[i] = bestLetter;
+            }
+            output.key = new String(key);
+            output.plainText = PortaCipher.decipherBellaco1552(cipherText, output.key);
+            output.fitness = FitnessCalculator.TetragramFitness(output.plainText);
+            if(output.fitness == oldFitness){break;}
+        }
+        output.isSuccessfull = true;
+        return output;
+    }
+
     public static CipherBreakerOutput bruteforce(String cipherText, double maxLength, int version){
-        CipherBreakerOutput output = new CipherBreakerOutput("PortaCipher", cipherText);
+        CipherBreakerOutput output = new CipherBreakerOutput("PortaCipher" + version, cipherText);
         output.fitness = FitnessCalculator.TetragramFitness(cipherText);
         for(int n = 1; n <= maxLength; n++){
             String[] possibleKeys = generateKeysPorta(n);
@@ -67,7 +136,7 @@ public class PortaCipherBreaker {
     }
 
     public static CipherBreakerOutput bruteforceWithWordlist(String cipherText, int version){
-        CipherBreakerOutput output = new CipherBreakerOutput("PortaCipher", cipherText);
+        CipherBreakerOutput output = new CipherBreakerOutput("PortaCipher"+ version, cipherText);
         output.fitness = FitnessCalculator.TetragramFitness(cipherText);
         for(int n = 0; n < Constants.wordlist.length; n++){
             String key = Constants.wordlist[n];
@@ -84,7 +153,7 @@ public class PortaCipherBreaker {
     }
 
     public static CipherBreakerOutput hillClimberAttack(String cipherText, int period, int version){
-        CipherBreakerOutput output = new CipherBreakerOutput("PortaCipher", cipherText);
+        CipherBreakerOutput output = new CipherBreakerOutput("PortaCipher"+ version, cipherText);
         output.fitness = FitnessCalculator.TetragramFitness(cipherText);
 
 
@@ -114,5 +183,6 @@ public class PortaCipherBreaker {
         output.isSuccessfull = true;
         return output;
     }
-    //TODO: attack using monograms
+
+    //TODO: attack using monograms both for version 1/2 and for modernized 1552
 }
