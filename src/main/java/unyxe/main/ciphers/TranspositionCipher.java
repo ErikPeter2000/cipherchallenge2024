06818cda6a00b255d34b5d8df5e124e5c05a17af
Unyxe <luckyboy7892official@gmail.com>
2024-09-20 09:56:36 +0100

    Moved permutation related function to the dedicated class.

--- src/main/java/unyxe/main/ciphers/TranspositionCipher.java
+++ src/main/java/unyxe/main/ciphers/TranspositionCipher.java
@@ -10,19 +10,4 @@ public class TranspositionCipher {
         System.out.println("[Transposition cipher Identifier] IoC: " + ioc);
         return mFrequency > 0.9;
     }
-    public static byte[] permutationComposition(byte[] permutationA, byte[] permutationB){
-        if(permutationA.length != permutationB.length) throw new IllegalArgumentException("Permutations must have equal length to be able to form a composition.");
-        byte[] product = new byte[permutationA.length];
-        for(int i = 0; i < permutationA.length;i++){
-            product[i] = permutationB[permutationA[i]];
-        }
-        return product;
-    }
-    public static byte[] permutationInverse(byte[] permutation){
-        byte[] inverse = new byte[permutation.length];
-        for(int i = 0; i < inverse.length;i++){
-            inverse[permutation[i]] = (byte)i;
-        }
-        return inverse;
-    }
 }
--- src/main/java/unyxe/main/utils/maths/Permutations.java
+++ src/main/java/unyxe/main/utils/maths/Permutations.java
@@ -79,6 +79,22 @@ public class Permutations {
         return factoradic;
     }
 
+    public static byte[] permutationComposition(byte[] permutationA, byte[] permutationB){
+        if(permutationA.length != permutationB.length) throw new IllegalArgumentException("Permutations must have equal length to be able to form a composition.");
+        byte[] product = new byte[permutationA.length];
+        for(int i = 0; i < permutationA.length;i++){
+            product[i] = permutationB[permutationA[i]];
+        }
+        return product;
+    }
+    public static byte[] permutationInverse(byte[] permutation){
+        byte[] inverse = new byte[permutation.length];
+        for(int i = 0; i < inverse.length;i++){
+            inverse[permutation[i]] = (byte)i;
+        }
+        return inverse;
+    }
+
     public static byte[] getNthPermutationOfMObjects(int n, int m){
         return getPermutationFromFactoradic(FactoradicNumbers.toFactoradicForm(n, m));
     }
